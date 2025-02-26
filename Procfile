32472cee1bcc0772dfd64557af5ee9bdb1b805a0
Ol Hughes <ol@fuzzyblue.monster>
2025-02-26 21:47:32 +0000

    Modified views.py DEBUG = False and added heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ols_project.wsgi
--- ols_project/settings.py
+++ ols_project/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-09h+3b3k1c!+z)sgs(=zp-ry3wwjoeb@todt74u!9x!^wb3*_7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
